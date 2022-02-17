4716d2e7997558411adfe8b149509b5d596c98de
awesomegamergame <awesomegamergames001@gmail.com>
2022-02-17 14:34:17 -0500

    Basic Funtions working 2/17/22 at 2:33PM

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace SchoolWallpaperChanger
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public string FileLocation;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Choose Image";
+            dialog.Filter = "All Graphics Types|*.bmp;*.jpg;*.jpeg;*.png;*.tif;*.tiff" + "BMP|*.bmp|GIF|*.gif|JPG|*.jpg;*.jpeg|PNG|*.png|TIFF|*.tif;*.tiff|";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                MessageBox.Show($"File Choosen {dialog.FileName}");
+                FileLocation = dialog.FileName;
+                Change.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Change.IsEnabled = false;
+            File.Copy(FileLocation, $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Microsoft\Windows\Themes\TranscodedWallpaper", true);
+            MessageBox.Show("This Program will now log you out to apply the wallpaper now. PLEASE SAVE YOUR WORK BEFORE HITING OK");
+            Process.Start("shutdown", "-l");
         }
     }
 }
