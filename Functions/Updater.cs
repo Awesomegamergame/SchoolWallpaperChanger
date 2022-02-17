3098212730fa44a7fc0bbd40a1599d7706bae26f
Awesomegamergame <awesomegamergames001@gmail.com>
2022-02-17 16:34:22 -0500

    Add my auto updater from agg productions

---
+++ Functions/Updater.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+using System.Reflection;
+
+namespace SchoolWallpaperChanger.Functions
+{
+    class Updater
+    {
+        public static string rootPath = Directory.GetCurrentDirectory();
+        public static string LauncherLink = "https://www.dropbox.com/s/27bwz9ct96qltlx/AGG%20Productions%20Temp.zip?dl=1";
+        public static string LauncherVerLink = "https://www.dropbox.com/s/l0s6jjask4paool/AGG%20Productions%20Version.txt?dl=1";
+        public static string startPath = @".\SchoolWallpaperChanger Temp";
+        public static string launcherZip = Path.Combine(rootPath, "SchoolWallpaperChanger Temp.zip");
+        public static int VersionDetector = 0;
+        public static Version onlineVersion;
+        public static Version localVersion;
+        public static void Update()
+        {
+            string exeOld = Path.Combine(rootPath, "SchoolWallpaperChanger.exe.old");
+            string pdbOld = Path.Combine(rootPath, "SchoolWallpaperChanger.pdb.old");
+
+            if (File.Exists(exeOld))
+            {
+                File.Delete(exeOld);
+            }
+            if (File.Exists(pdbOld))
+            {
+                File.Delete(pdbOld);
+            }
+            if (Directory.Exists(startPath))
+            {
+                Directory.Delete(startPath);
+            }
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            System.Version version = assembly.GetName().Version;
+            string versionS = version.ToString();
+            versionS = versionS.Substring(0, versionS.Length - 2);
+            localVersion = new Version(versionS);
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                onlineVersion = new Version(webClient.DownloadString(LauncherVerLink));
+                if (onlineVersion.IsDifferentThan(localVersion))
+                {
+                    VersionDetector += 1;
+                    MainWindow.UpdateScreen_Image.Visibility = Visibility.Visible;
+                    MainWindow.Yes_Button.Visibility = Visibility.Visible;
+                    MainWindow.No_Button.Visibility = Visibility.Visible;
+                    MainWindow.UpdateText1_Label.Visibility = Visibility.Visible;
+                    MainWindow.UpdateText2_Label.Visibility = Visibility.Visible;
+                    MainWindow.LocalVersionObject.Visibility = Visibility.Visible;
+                    MainWindow.OnlineVersionObject.Visibility = Visibility.Visible;
+                    MainWindow.LocalVersionNumberObject.Content = localVersion;
+                    MainWindow.OnlineVersionNumberObject.Content = onlineVersion;
+                    MainWindow.LocalVersionNumberObject.Visibility = Visibility.Visible;
+                    MainWindow.OnlineVersionNumberObject.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error checking for game updates: {ex}");
+            }
+        }
+
+        public static void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                FileDownloader LauncherDownload = new FileDownloader();
+                WebClient webClient = new WebClient();
+                if (!_isUpdate)
+                {
+                    _onlineVersion = new Version(webClient.DownloadString(LauncherVerLink));
+                }
+                LauncherDownload.DownloadFileAsync(LauncherLink, launcherZip, _onlineVersion);
+                LauncherDownload.DownloadProgressChanged += LauncherDownload_DownloadProgressChanged;
+                LauncherDownload.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error installing game files: {ex}");
+            }
+        }
+
+        private static void LauncherDownload_DownloadProgressChanged(object sender, FileDownloader.DownloadProgress progress)
+        {
+            MainWindow.UpdateDownloadBar.Value = progress.ProgressPercentage;
+        }
+
+        private static void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            string Exe = Path.Combine(rootPath, "SchoolWallpaperChanger.exe");
+            string pdb = Path.Combine(rootPath, "SchoolWallpaperChanger.pdb");
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                if (!Directory.Exists(startPath))
+                {
+                    if (File.Exists(Exe))
+                    {
+                        File.Move(@".\SchoolWallpaperChanger.exe", @".\SchoolWallpaperChanger.exe.old");
+                    }
+                    if (File.Exists(pdb))
+                    {
+                        File.Move(@".\SchoolWallpaperChanger.pdb", @".\SchoolWallpaperChanger.pdb.old");
+                    }
+                    try
+                    {
+                        //Declare a temporary path to unzip your files
+                        string tempPath = Path.Combine(Directory.GetCurrentDirectory(), "tempUnzip");
+                        string extractPath = Directory.GetCurrentDirectory();
+                        ZipFile.ExtractToDirectory(launcherZip, tempPath);
+
+                        //build an array of the unzipped files
+                        string[] files = Directory.GetFiles(tempPath);
+
+                        foreach (string file in files)
+                        {
+                            FileInfo f = new FileInfo(file);
+                            //Check if the file exists already, if so delete it and then move the new file to the extract folder
+                            if (File.Exists(Path.Combine(extractPath, f.Name)))
+                            {
+                                File.Delete(Path.Combine(extractPath, f.Name));
+                                File.Move(f.FullName, Path.Combine(extractPath, f.Name));
+                            }
+                            else
+                            {
+                                File.Move(f.FullName, Path.Combine(extractPath, f.Name));
+                            }
+                        }
+                        //Delete the temporary directory.
+                        Directory.Delete(tempPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                    File.Delete(launcherZip);
+                    Process.Start(Exe);
+                    Application.Current.Shutdown();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        public static void UpdaterVersion()
+        {
+            if(VersionDetector == 1)
+            {
+                InstallGameFiles(true, onlineVersion);
+            }
+            else if(VersionDetector == 2)
+            {
+                InstallGameFiles(false, Version.zero);
+            }
+        }
+    }
+
+    public struct Version
+    {
+        internal static Version zero = new Version(0, 0, 0);
+
+        private readonly short major;
+        private readonly short minor;
+        private readonly short subMinor;
+
+        internal Version(short _major, short _minor, short _subMinor)
+        {
+            major = _major;
+            minor = _minor;
+            subMinor = _subMinor;
+        }
+        internal Version(string _version)
+        {
+            string[] _versionStrings = _version.Split('.');
+            if (_versionStrings.Length != 3)
+            {
+                major = 0;
+                minor = 0;
+                subMinor = 0;
+                return;
+            }
+
+            major = short.Parse(_versionStrings[0]);
+            minor = short.Parse(_versionStrings[1]);
+            subMinor = short.Parse(_versionStrings[2]);
+        }
+        internal bool IsDifferentThan(Version _otherVersion)
+        {
+            if (major != _otherVersion.major)
+            {
+                return true;
+            }
+            else
+            {
+                if (minor != _otherVersion.minor)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (subMinor != _otherVersion.subMinor)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{subMinor}";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using Microsoft.Win32;
 using System.Diagnostics;
+using SchoolWallpaperChanger.Functions;
 
 namespace SchoolWallpaperChanger
 {
@@ -10,12 +12,25 @@ namespace SchoolWallpaperChanger
     {
         public string FileLocation;
 
+        public static Button Yes_Button;
+        public static Button No_Button;
+        public static Image UpdateScreen_Image;
+        public static Label UpdateText1_Label;
+        public static Label UpdateText2_Label;
+        public static ProgressBar GameDownloadBar;
+        public static ProgressBar UpdateDownloadBar;
+        public static Label LocalVersionObject, LocalVersionNumberObject;
+        public static Label OnlineVersionObject, OnlineVersionNumberObject;
+
         public MainWindow()
         {
+            CheckInternet.CheckInternetState();
             InitializeComponent();
+            if (CheckInternet.IsOnline)
+                Updater.Update();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Select_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog();
             dialog.Title = "Choose Image";
@@ -31,12 +46,88 @@ namespace SchoolWallpaperChanger
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Change_Click(object sender, RoutedEventArgs e)
         {
             Change.IsEnabled = false;
             File.Copy(FileLocation, $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\Microsoft\Windows\Themes\TranscodedWallpaper", true);
             MessageBox.Show("This Program will now log you out to apply the wallpaper now. PLEASE SAVE YOUR WORK BEFORE HITING OK");
             Process.Start("shutdown", "-l");
         }
+        #region Updates
+        private void No_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateScreen_Image.Visibility = Visibility.Collapsed;
+            Yes_Button.Visibility = Visibility.Collapsed;
+            No_Button.Visibility = Visibility.Collapsed;
+            UpdateText1_Label.Visibility = Visibility.Collapsed;
+            UpdateText2_Label.Visibility = Visibility.Collapsed;
+            LocalVersionObject.Visibility = Visibility.Collapsed;
+            LocalVersionNumberObject.Visibility = Visibility.Collapsed;
+            OnlineVersionNumberObject.Visibility = Visibility.Collapsed;
+            OnlineVersionObject.Visibility = Visibility.Collapsed;
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e)
+        {
+            Yes_Button.Visibility = Visibility.Collapsed;
+            No_Button.Visibility = Visibility.Collapsed;
+            UpdateDownloadBar.Visibility = Visibility.Visible;
+            if (Updater.VersionDetector == 1)
+            {
+                Updater.UpdaterVersion();
+                Updater.VersionDetector = 0;
+            }
+            else if (Updater.VersionDetector == 2)
+            {
+                Updater.UpdaterVersion();
+                Updater.VersionDetector = 0;
+            }
+        }
+        private void Yes_Initialized(object sender, EventArgs e)
+        {
+            Yes_Button = (Button)sender;
+        }
+        private void No_Initialized(object sender, EventArgs e)
+        {
+            No_Button = (Button)sender;
+        }
+        private void UpdateText1_Initialized(object sender, EventArgs e)
+        {
+            UpdateText1_Label = (Label)sender;
+        }
+        private void UpdateText2_Initialized(object sender, EventArgs e)
+        {
+            UpdateText2_Label = (Label)sender;
+        }
+        private void UpdateScreen_Initialized(object sender, EventArgs e)
+        {
+            UpdateScreen_Image = (Image)sender;
+        }
+        private void ProgressBar_Initialized(object sender, EventArgs e)
+        {
+            GameDownloadBar = (ProgressBar)sender;
+        }
+        private void UpdateBar_Initialized(object sender, EventArgs e)
+        {
+            UpdateDownloadBar = (ProgressBar)sender;
+        }
+        private void LocalVersionNumber_Initialized(object sender, EventArgs e)
+        {
+            LocalVersionNumberObject = (Label)sender;
+        }
+        private void LocalVersion_Initialized(object sender, EventArgs e)
+        {
+            LocalVersionObject = (Label)sender;
+        }
+        private void OnlineVersionNumber_Initialized(object sender, EventArgs e)
+        {
+            OnlineVersionNumberObject = (Label)sender;
+        }
+
+        private void OnlineVersion_Initialized(object sender, EventArgs e)
+        {
+            OnlineVersionObject = (Label)sender;
+        }
+        #endregion
     }
 }
