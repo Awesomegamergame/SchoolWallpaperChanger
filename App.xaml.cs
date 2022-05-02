6ea605f771bab2b7dab4e82b8a52925df5dc3ff8
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2022-05-02 07:55:34 -0400

    Fixed multiple apps being able to start at the same time

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,10 @@
-﻿using SchoolWallpaperChanger.Functions;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
+﻿using System;
+using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SchoolWallpaperChanger
 {
@@ -16,13 +12,14 @@ namespace SchoolWallpaperChanger
     {
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             string[] args = Environment.GetCommandLineArgs();
-            var mutex = new Mutex(true, "SchoolWallpaperChanger", out bool aIsNewInstance);
-            if (!aIsNewInstance)
+            var exists = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)).Count() > 1;
+            if (exists)
             {
                 Process current = Process.GetCurrentProcess();
                 foreach (Process process in Process.GetProcessesByName(current.ProcessName))
